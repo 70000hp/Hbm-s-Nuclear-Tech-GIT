98a4cce80d0493a6b7e00bdf612912f2364d9c79
70000hp <105080577+70000hp@users.noreply.github.com>
2025-06-25 14:43:07 -0400

    rocks and stones may break my stones
    but beer never stoned me

--- src/main/java/com/hbm/tileentity/machine/oil/TileEntityMachineLiquefactor.java
+++ src/main/java/com/hbm/tileentity/machine/oil/TileEntityMachineLiquefactor.java
@@ -43,7 +43,7 @@ public class TileEntityMachineLiquefactor extends TileEntityMachineBase implemen
 	public static final int usageBase = 500;
 	public int usage;
 	public int progress;
-	public static final int processTimeBase = 100;
+	public static final int processTimeBase = 40;
 	public int processTime;
 
 	public FluidTank tank;
