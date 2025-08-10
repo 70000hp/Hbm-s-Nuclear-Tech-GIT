346b4131a6b77142c5cc677b1d9f50f5e5ab951d
70000hp <105080577+70000hp@users.noreply.github.com>
2025-08-10 13:29:53 -0400

    kill

--- src/main/java/com/hbm/inventory/recipes/FractionRecipes.java
+++ src/main/java/com/hbm/inventory/recipes/FractionRecipes.java
@@ -38,7 +38,7 @@ public class FractionRecipes extends SerializableRecipe {
 		fractions.put(Fluids.EGG,				new Pair(new FluidStack(Fluids.CHOLESTEROL,				50),		new FluidStack(Fluids.RADIOSOLVENT,			50)));
 		fractions.put(Fluids.OIL_COKER,			new Pair(new FluidStack(Fluids.CRACKOIL,				30),		new FluidStack(Fluids.HEATINGOIL,			70)));
 		fractions.put(Fluids.NAPHTHA_COKER,		new Pair(new FluidStack(Fluids.NAPHTHA_CRACK,			75),		new FluidStack(Fluids.LIGHTOIL_CRACK,		25)));
-		fractions.put(Fluids.GAS_COKER,			new Pair(new FluidStack(Fluids.AROMATICS,				25),		new FluidStack(Fluids.CARBONDIOXIDE,		75)));
+		fractions.put(Fluids.GAS_COKER,			new Pair(new FluidStack(Fluids.UNSATURATEDS,				25),		new FluidStack(Fluids.CARBONDIOXIDE,		75)));
 		fractions.put(Fluids.CHLOROCALCITE_MIX,	new Pair(new FluidStack(Fluids.CHLOROCALCITE_CLEANED,	50),		new FluidStack(Fluids.COLLOID,				50)));
 		fractions.put(Fluids.BAUXITE_SOLUTION,	new Pair(new FluidStack(Fluids.REDMUD,					50),		new FluidStack(Fluids.SODIUM_ALUMINATE,		50)));
 	}
